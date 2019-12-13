3970856d00bdaf75a3e88f430fef7d61ff537e18
jneilliii <jneilliii+github@gmail.com>
2019-12-13 17:00:57 -0500

    Initial working version for standard temperature readings.

---
+++ octoprint_plotlytempgraph/__init__.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class PlotlytempgraphPlugin(octoprint.plugin.SettingsPlugin,
+                            octoprint.plugin.AssetPlugin,
+                            octoprint.plugin.TemplatePlugin):
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			# put your plugin's default settings here
+		)
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		return dict(
+			js=["js/plotly-latest.min.js","js/plotlytempgraph.js"]
+		)
+
+	## Temperatures received hook
+
+	def temp_received(self, comm, parsed_temps):
+		self._plugin_manager.send_plugin_message(self._identifier, parsed_temps)
+		return parsed_temps
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		return dict(
+			plotlytempgraph=dict(
+				displayName="Plotly Temp Graph",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-PlotlyTempGraph",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-PlotlyTempGraph/archive/{target_version}.zip"
+			)
+		)
+
+__plugin_name__ = "Plotly Temp Graph"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = PlotlytempgraphPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+		"octoprint.comm.protocol.temperatures.received": __plugin_implementation__.temp_received
+	}
+
---
+++ octoprint_plotlytempgraph/static/js/plotlytempgraph.js
@@ -0,0 +1,61 @@
+/*
+ * View model for OctoPrint-PlotlyTempGraph
+ *
+ * Author: jneilliii
+ * License: AGPLv3
+ */
+$(function() {
+	function PlotlytempgraphViewModel(parameters) {
+		var self = this;
+
+		self.loginStateViewModel = parameters[0];
+
+		self.data = [];
+		self.layout = {
+			autosize: true,
+			showlegend: true,
+			legend: {"orientation": "h"},
+			xaxis: { type:"date", tickformat:"%H:%M:%S", automargin: true, title: {standoff: 0},linecolor: 'black', linewidth: 2, mirror: true},
+			yaxis: { type:"linear", automargin: true, title: {standoff: 0},linecolor: 'black', linewidth: 2, mirror: true },
+			margin: {l:0,r:30,b:0,t:20,pad:5}
+		};
+		self.options = {
+			showLink: false,
+			sendData: false,
+			displaylogo: false,
+			displayModeBar: false,
+			editable: false,
+			showTips: false
+		};
+
+		self.trace_exists = function(){
+			
+		}
+
+		Plotly.newPlot('plotlytempgraph', self.data, self.layout, self.options);
+
+		self.onDataUpdaterPluginMessage = function(plugin, data) {
+			if (plugin != "plotlytempgraph") {
+				return;
+			}
+			var timestamp = new Date();
+
+			var gd = document.getElementById('plotlytempgraph').data;
+			
+			for (var key in data) {
+				var index = gd.findIndex( ({ name }) => name === key );
+				if (index < 0) {
+					Plotly.addTraces('plotlytempgraph',{name:key,x:[[timestamp]],y:[[data[key][0]]],mode: 'lines'});
+				} else {
+					Plotly.extendTraces('plotlytempgraph', {x: [[timestamp]], y: [[data[key][0]]]}, [index]);
+				}
+			}
+		};
+	}
+
+	OCTOPRINT_VIEWMODELS.push({
+		construct: PlotlytempgraphViewModel,
+		dependencies: [ "loginStateViewModel" ],
+		elements: [ /* ... */ ]
+	});
+});
