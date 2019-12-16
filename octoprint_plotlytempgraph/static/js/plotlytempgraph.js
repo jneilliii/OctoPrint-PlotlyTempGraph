0755dd02414513c18a5c179a7402ac3439f345c9
jneilliii <jneilliii+octoprint@gmail.com>
2019-12-16 13:14:42 -0500

    why the colors aren't working?

--- octoprint_plotlytempgraph/static/js/plotlytempgraph.js
+++ octoprint_plotlytempgraph/static/js/plotlytempgraph.js
@@ -43,9 +43,11 @@ $(function() {
 				var actual_index = gd.findIndex( ({ name }) => name === key + ' Actual');
 				var target_index = gd.findIndex( ({ name }) => name === key + ' Target');
 				if (actual_index < 0) {
+					console.log('Adding line for "' + key + ' Actual" with color "' + d3colors(actual_index));
 					Plotly.addTraces('plotlytempgraph',{name:key + ' Actual',x:[[timestamp]],y:[[data[key][0]]],mode: 'lines'});
 					if(typeof data[key][1] !== 'undefined' && target_index < 0) {
 						var target_color = pusher.color(d3colors(actual_index)).tint(0.5).html();
+						console.log('Adding line for "' + key + ' Target" with color "' + target_color);
 						Plotly.addTraces('plotlytempgraph',{name:key + ' Target',x:[[timestamp]],y:[[data[key][1]]],mode: 'lines',line:{color: target_color}});
 					}
 				} else {
