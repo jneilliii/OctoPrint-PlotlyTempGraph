abda6b84792eb3e45484694bf3177a97907e8a2c
jneilliii <jneilliii+github@gmail.com>
2021-01-24 00:02:36 -0500

    fix indentation errors

--- octoprint_plotlytempgraph/__init__.py
+++ octoprint_plotlytempgraph/__init__.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
+
 class PlotlytempgraphPlugin(octoprint.plugin.SettingsPlugin,
-                            octoprint.plugin.AssetPlugin,
-                            octoprint.plugin.TemplatePlugin):
+							octoprint.plugin.AssetPlugin,
+							octoprint.plugin.TemplatePlugin):
 
 	##~~ SettingsPlugin mixin
 
@@ -24,7 +25,8 @@ class PlotlytempgraphPlugin(octoprint.plugin.SettingsPlugin,
 	def get_template_configs(self):
 		return [
 			dict(type="tab", name="Temperature", template="plotlytempgraph_tab.jinja2", replaces="temperature"),
-			dict(type="settings", template="plotlytempgraph_settings.jinja2", replaces="temperature", custom_bindings=False),
+			dict(type="settings", template="plotlytempgraph_settings.jinja2", replaces="temperature",
+				 custom_bindings=False),
 			dict(type="generic", template="plotlytempgraph.jinja2")
 		]
 
@@ -41,28 +43,30 @@ class PlotlytempgraphPlugin(octoprint.plugin.SettingsPlugin,
 				user="jneilliii",
 				repo="OctoPrint-PlotlyTempGraph",
 				current=self._plugin_version,
-                stable_branch=dict(
-                    name="Stable", branch="master", comittish=["master"]
-                ),
-                prerelease_branches=[
-                    dict(
-                        name="Release Candidate",
-                        branch="rc",
-                        comittish=["rc", "master"],
-                    ),
-                    dict(
-                        name="Development",
-                        branch="develop",
-                        comittish=["develop", "rc", "master"],
-                    )
-                ],
+				stable_branch=dict(
+					name="Stable", branch="master", comittish=["master"]
+				),
+				prerelease_branches=[
+					dict(
+						name="Release Candidate",
+						branch="rc",
+						comittish=["rc", "master"],
+					),
+					dict(
+						name="Development",
+						branch="develop",
+						comittish=["develop", "rc", "master"],
+					)
+				],
 				# update method: pip
 				pip="https://github.com/jneilliii/OctoPrint-PlotlyTempGraph/archive/{target_version}.zip"
 			)
 		)
 
+
 __plugin_name__ = "Plotly Temp Graph"
 __plugin_pythoncompat__ = ">=2.7,<4"
+
 
 def __plugin_load__():
 	global __plugin_implementation__
@@ -72,4 +76,3 @@ def __plugin_load__():
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 	}
-
